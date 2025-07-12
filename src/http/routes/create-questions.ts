89d8ce6879a9999bcd8181ccf841544176270348
NicolasLOrtiz <contato.nicolol@gmail.com>
2025-07-11 21:37:41 -0300

    Aula 03 - Adiciona suporte para upload e transcrição de áudio, incluindo a criação da tabela audio_chunks e integração com a API Gemini

--- src/http/routes/create-questions.ts
+++ src/http/routes/create-questions.ts
@@ -1,8 +1,11 @@
+import {and, eq, sql} from 'drizzle-orm';
 import type {FastifyPluginCallbackZod} from 'fastify-type-provider-zod';
 import {z} from 'zod/v4';
 import {db} from '../../db/connection.ts';
 import {schema} from '../../db/schema/index.ts';
-// import {generateAnswer, generateEmbeddings} from '../../services/gemini.ts';
+import {generateAnswer, generateEmbeddings} from '../../services/gemini.ts';
+
+const SIMILARITY_THRESHOLD = 0.7;
 
 export const createQuestionRoute: FastifyPluginCallbackZod = (app) => {
   app.post(
@@ -21,35 +24,36 @@ export const createQuestionRoute: FastifyPluginCallbackZod = (app) => {
       const { roomId } = request.params;
       const { question } = request.body;
 
-      // const embeddings = await generateEmbeddings(question);
+      const embeddings = await generateEmbeddings(question);
 
-      // const embeddingsAsString = `[${embeddings.join(',')}]`;
+      const embeddingsAsString = `[${embeddings.join(',')}]`;
 
-      // const chunks = await db
-      //   .select({
-      //     id: schema.audioChunks.id,
-      //     transcription: schema.audioChunks.transcription,
-      //     similarity: sql<number>`1 - (${schema.audioChunks.embeddings} <=> ${embeddingsAsString}::vector)`,
-      //   })
-      //   .from(schema.audioChunks)
-      //   .where(
-      //     and(
-      //       eq(schema.audioChunks.roomId, roomId),
-      //       sql`1 - (${schema.audioChunks.embeddings} <=> ${embeddingsAsString}::vector) > 0.7`
-      //     )
-      //   )
-      //   .orderBy(
-      //     sql`${schema.audioChunks.embeddings} <=> ${embeddingsAsString}::vector`
-      //   )
-      //   .limit(3);
+      const chunks = await db
+        .select({
+          id: schema.audioChunks.id,
+          transcription: schema.audioChunks.transcription,
+          similarity: sql<number>`1 - (${schema.audioChunks.embeddings} <=> ${embeddingsAsString}::vector)`,
+        })
+        .from(schema.audioChunks)
+        .where(
+          and(
+            eq(schema.audioChunks.roomId, roomId),
+            sql`1 - (${schema.audioChunks.embeddings} <=> ${embeddingsAsString}::vector) >
+            ${SIMILARITY_THRESHOLD}`
+          )
+        )
+        .orderBy(
+          sql`${schema.audioChunks.embeddings} <=> ${embeddingsAsString}::vector`
+        )
+        .limit(3);
 
-      const answer: string | null = null;
+      let answer: string | null = null;
 
-      // if (chunks.length > 0) {
-      //   const transcriptions = chunks.map((chunk) => chunk.transcription);
-      //
-      //   answer = await generateAnswer(question, transcriptions);
-      // }
+      if (chunks.length > 0) {
+        const transcriptions = chunks.map((chunk) => chunk.transcription);
+
+        answer = await generateAnswer(question, transcriptions);
+      }
 
       const result = await db
         .insert(schema.questions)
@@ -59,7 +63,7 @@ export const createQuestionRoute: FastifyPluginCallbackZod = (app) => {
       const insertedQuestion = result[0];
 
       if (!insertedQuestion) {
-        throw new Error('Failed to create new room.');
+        throw new Error('Failed to create question');
       }
 
       return reply.status(201).send({
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import {fastifyCors} from '@fastify/cors';
+import fastifyMultipart from '@fastify/multipart';
 import {fastify} from 'fastify';
 import {serializerCompiler, validatorCompiler, type ZodTypeProvider,} from 'fastify-type-provider-zod';
 import {env} from './env.ts';
@@ -6,6 +7,7 @@ import {createQuestionRoute} from './http/routes/create-questions.ts';
 import {createRoomRoute} from './http/routes/create-room.ts';
 import {getRoomQuestions} from './http/routes/get-room-questions.ts';
 import {getRoomsRoute} from './http/routes/get-rooms.ts';
+import {uploadAudioRoute} from './http/routes/upload-audio.ts';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -13,6 +15,7 @@ app.register(fastifyCors, {
   origin: 'http://localhost:5173',
 });
 
+app.register(fastifyMultipart);
 app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
 
@@ -24,5 +27,6 @@ app.register(getRoomsRoute);
 app.register(createRoomRoute);
 app.register(getRoomQuestions);
 app.register(createQuestionRoute);
+app.register(uploadAudioRoute);
 
 app.listen({ port: env.PORT });
